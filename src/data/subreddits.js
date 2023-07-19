fe26cb28bbda5f4c4d49dcf6ab947a3b7bd076fc
Chris Mastris <cmastris@hotmail.com>
2023-07-19 21:57:57 +0100

    Add source subreddit name data

---
+++ src/data/subreddits.js
@@ -0,0 +1,10 @@
+export const allSubreddits = [
+  "AskHistorians",
+  "askscience",
+  "EverythingScience",
+  "explainlikeimfive",
+  "history",
+  "IWantToLearn",
+  "science",
+  "todayilearned",
+];
