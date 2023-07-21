790a8c2982b8455aa6ee703fee3d13c650ebb964
Chris Mastris <cmastris@hotmail.com>
2023-07-21 13:34:52 +0100

    Configure React-Redux integration testing (RTL)

--- src/App.test.js
+++ src/App.test.js
@@ -1,1 +1,13 @@
+import { screen } from '@testing-library/react';
+
+import { renderWithProviders } from './setupTests';
 import App from './App';
+
+test('App renders without crashing', () => {
+  renderWithProviders(<App />);
+});
+
+test('App dummy content is rendered', () => {
+  renderWithProviders(<App />);
+  screen.getByText(/Hello World!/i);
+});
--- src/setupTests.js
+++ src/setupTests.js
@@ -1,5 +1,27 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+import React from "react";
+import { render } from "@testing-library/react";
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+
+import postListingsReducer from "./features/postListings/postListingsSlice";
+
+// https://redux.js.org/usage/writing-tests#integration-testing-connected-components-and-redux-logic
+// https://testing-library.com/docs/react-testing-library/setup/#custom-render
+export function renderWithProviders(
+  ui,
+  {
+    preloadedState = {},
+    // Automatically create a store instance if no store was passed in
+    store = configureStore({ reducer: { 
+      postListings: postListingsReducer,
+    }, preloadedState }),
+    ...renderOptions
+  } = {}
+) {
+  function Wrapper({ children }) {
+    return <Provider store={store}>{children}</Provider>;
+  }
+
+  // Return an object with the store and all of React Testing Library's query functions
+  return { store, ...render(ui, { wrapper: Wrapper, ...renderOptions }) };
+}
