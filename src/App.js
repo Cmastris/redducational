0cd417da6c110462bc77f1ec4f4329a117de251d
Chris Mastris <cmastris@hotmail.com>
2023-07-21 17:48:35 +0100

    Load source subreddit names into state

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+
+import { allSubreddits } from "./data/subreddits";
+import { addSubreddit } from "./features/postListings/postListingsSlice";
 
 function App() {
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    // Load array of source subreddit names into state
+    allSubreddits.forEach(subreddit => {
+      dispatch(addSubreddit({ name: subreddit }));
+    });
+  }, []);
+
   return (
     <div className="App">
       Hello World!
