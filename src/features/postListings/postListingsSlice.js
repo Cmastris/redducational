908c6804a184c9881d82be48e3b9a3c32eb61a72
Chris Mastris <cmastris@hotmail.com>
2023-07-22 18:26:06 +0100

    Refactor individual sub fetch into separate thunk

--- src/features/postListings/postListingsSlice.js
+++ src/features/postListings/postListingsSlice.js
@@ -38,22 +38,29 @@ export const { addListing } = postListings.actions;
 export const selectSubreddits = (state) => state.postListings.subreddits;
 export const selectSubsLoadedStatus = (state) => state.postListings.subsLoaded;
 
-// https://redux.js.org/usage/writing-logic-thunks
-// https://redux.js.org/tutorials/essentials/part-5-async-logic
-export const fetchListingsData = () => async (dispatch, getState) => {
-  const state = getState();
-  const subreddits = selectSubreddits(state);
-  const sub = subreddits['science'].name;
-
+const fetchSubPostData = (sub) => async (dispatch) => {
   try {
     const json = await fetchSubTopPosts(sub, 'day');
     console.log(json);
     // TODO: Dispatch post data to store
     dispatch(changeSubRetrievedStatus({ name: sub, retrieved: true }));
     // TODO: Create array of post IDs
+    return [1, 2, 3];
   } catch(e) {
     console.log(e);
+    return [];
   }
+};
+
+export const fetchListingsData = () => async (dispatch, getState) => {
+  // https://redux.js.org/usage/writing-logic-thunks
+  // https://redux.js.org/tutorials/essentials/part-5-async-logic
+  const state = getState();
+  const subreddits = selectSubreddits(state);
+  const sub = subreddits['science'].name;
+  
+  const ids = await dispatch(fetchSubPostData(sub));
+  console.log(ids);
 };
 
 export default postListings.reducer;
