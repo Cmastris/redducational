17eb379529d3b253b8bc61b869b25df62f6cf07e
Chris Mastris <cmastris@hotmail.com>
2023-07-22 11:23:48 +0100

    Update `subsLoaded` in App

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 import { allSubreddits } from "./data/subreddits";
-import { addSubreddit } from "./features/postListings/postListingsSlice";
+import { addSubreddit, selectSubreddits, changeSubsLoadedStatus, selectSubsLoadedStatus } from "./features/postListings/postListingsSlice";
 
 function App() {
 
   const dispatch = useDispatch();
+  const subsLoaded = useSelector(selectSubsLoadedStatus);
 
   useEffect(() => {
     // Load array of source subreddit names into state
     allSubreddits.forEach(subreddit => {
       dispatch(addSubreddit({ name: subreddit }));
     });
+    dispatch(changeSubsLoadedStatus({ loaded: true }))
   }, []);
 
   return (
