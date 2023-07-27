e3b446ac8594376a8b7ddb5e122b5ec52688e387
Chris Mastris <cmastris@hotmail.com>
2023-07-27 21:52:02 +0100

    Create `Header` component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import SearchBar from "../SearchBar/SearchBar";
+
+export default function Header() {
+
+  return (
+    <header>
+      <h1>redducational</h1>
+      <SearchBar />
+    </header>
+  );
+}
