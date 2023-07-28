579c9a7af4cf4b63c31673817eac10fb0fb03887
Chris Mastris <cmastris@hotmail.com>
2023-07-28 13:14:34 +0100

    Style `SearchBar`

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,13 +1,20 @@
+import { MdSearch } from "react-icons/md";
+
+import styles from "./SearchBar.module.css";
+
 export default function SearchBar() {
   // TODO: implement search functionality
   return (
-    <form>
+    <form className={styles.searchForm}>
       <input
+        className={styles.searchBar}
         type="text"
         placeholder="Search posts..."
         aria-label="Enter search query"
       />
-      <input type="submit" aria-label="Search" />
+      <button className={styles.searchButton} type="submit" aria-label="Search">
+        <MdSearch color={'rgb(190, 195, 200)'} size={40} />
+      </button>
     </form>
   );
 }
