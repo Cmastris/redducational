8ddf58142637da652f95b9b2cb21244bf2146b02
Chris Mastris <cmastris@hotmail.com>
2023-07-28 18:02:51 +0100

    Move routing config to `routing.js`

--- src/index.js
+++ src/index.js
@@ -1,38 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
 
-import { categories } from "./data/categories";
+import { router } from "./routing";
 import store from "./store";
 
-import App from "./App/App";
-import ErrorPage from "./components/ErrorPage/ErrorPage";
-import PostListing from "./features/postListings/PostListing";
 import "./index.css";
-
-const categoryListings = categories.map(category => {
-  return {
-    path: "categories/" + category.path,
-    element: <PostListing name={category.name} />,
-  }
-});
-
-// https://reactrouter.com/en/main/routers/create-browser-router
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        path: "",
-        element: <PostListing name="All" />,
-      },
-      ...categoryListings
-    ],
-  },
-]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
---
+++ src/routing.js
@@ -0,0 +1,31 @@
+import { createBrowserRouter } from "react-router-dom";
+
+import { categories } from "./data/categories";
+
+import App from "./App/App";
+import ErrorPage from "./components/ErrorPage/ErrorPage";
+import PostListing from "./features/postListings/PostListing";
+
+
+const categoryListings = categories.map(category => {
+  return {
+    path: "categories/" + category.path,
+    element: <PostListing name={category.name} />,
+  }
+});
+
+// https://reactrouter.com/en/main/routers/create-browser-router
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "",
+        element: <PostListing name="All" />,
+      },
+      ...categoryListings
+    ],
+  },
+]);
