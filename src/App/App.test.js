0056a902877b3826ae50b0460d040ca045fe4fdc
Chris Mastris <cmastris@hotmail.com>
2023-07-29 10:57:11 +0100

    Add homepage feed post inclusion test

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -6,6 +6,7 @@ import PostListing from "../features/postListings/PostListing";
 
 import { categoryListingRoutes } from "../routing";
 import { renderWithProviders } from "../testSetup/setupTests";
+import { testState1 } from "../testSetup/testState";
 
 // https://testing-library.com/docs/react-testing-library/intro
 // https://jestjs.io/docs/asynchronous
@@ -31,7 +32,13 @@ test('Category listing routing', () => {
   expect(screen.getByText("Science")).toBeInTheDocument();
 });
 
-// test('Waits for async code and fails', async () => {
-//   renderWithProviders(<App />);
-//   expect(await screen.findByText('Not present')).toBeInTheDocument();
-// });
+test('Posts are not filtered when rendered in the `All` listing feed', () => {
+  renderWithProviders(<PostListing name="All" />, { preloadedState: testState1 });
+  // TODO: change to post title after implementing post item component
+  expect(screen.getByText("1")).toBeInTheDocument();
+  expect(screen.getByText("2")).toBeInTheDocument();
+  expect(screen.getByText("3")).toBeInTheDocument();
+  expect(screen.getByText("4")).toBeInTheDocument();
+  expect(screen.getByText("5")).toBeInTheDocument();
+  expect(screen.getByText("6")).toBeInTheDocument();
+});
---
+++ src/testSetup/testState.js
@@ -0,0 +1,58 @@
+export const testState1 = {
+  postListings: {
+    staticDataLoaded: true,
+    subreddits: {},
+    listingsLoaded: true,
+    listings: {
+      "All": {
+        name: "All",
+        includedSubs: ["Sub1", "Sub2", "Sub3"],
+        postIds: ["1", "2", "3", "4", "5", "6"]
+      },
+      "Cat 1": {
+        name: "Cat 1",
+        includedSubs: ["Sub1", "Sub3"],
+        postIds: []
+      },
+      "Cat 2": {
+        name: "Cat 2",
+        includedSubs: ["Sub2"],
+        postIds: []
+      }
+    }
+  },
+  postContent: { 
+    posts: {
+      "1": {
+        id: "1",
+        subreddit: "Sub1",
+        title: "Post 1"
+      },
+      "2": {
+        id: "2",
+        subreddit: "Sub2",
+        title: "Post 2"
+      },
+      "3": {
+        id: "3",
+        subreddit: "Sub3",
+        title: "Post 3"
+      },
+      "4": {
+        id: "4",
+        subreddit: "Sub1",
+        title: "Post 4"
+      },
+      "5": {
+        id: "5",
+        subreddit: "Sub2",
+        title: "Post 5"
+      },
+      "6": {
+        id: "6",
+        subreddit: "Sub3",
+        title: "Post 6"
+      },
+    }
+  }
+};
