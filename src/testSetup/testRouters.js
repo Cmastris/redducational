fc75fb0dcf7fa501bc48fd9b94b9979e3a137b56
Chris Mastris <cmastris@hotmail.com>
2023-07-29 19:54:42 +0100

    Add `createRouterProvider` testing helper function

--- src/testSetup/testRouters.js
+++ src/testSetup/testRouters.js
@@ -1,4 +1,4 @@
-import { createMemoryRouter } from "react-router-dom";
+import { createMemoryRouter, RouterProvider } from "react-router-dom";
 
 import App from "../App/App";
 
@@ -6,3 +6,14 @@ export const simpleAppRouter = createMemoryRouter(
   [{ path: "/", element: <App /> }],
   { initialEntries: ["/"] }
 );
+
+export function createRouterProvider(component, path = "/", initialEntries = ["/"]) {
+  // Create a simple `RouterProvider` for tests that don't involve routing
+  // This is required when rendering components that use React Router functions/components
+  // E.g. `useSearchParams()` (PostListing) and `Form` (SearchBar)
+  const router = createMemoryRouter(
+    [{ path: path, element: component }],
+    { initialEntries: initialEntries }
+  );
+  return <RouterProvider router={router} />
+}
