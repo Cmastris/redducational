fca63d78d3d998da4621e970217b4945a03cd0f2
Chris Mastris <cmastris@hotmail.com>
2023-07-30 14:03:53 +0100

    Refactor post listing filtering functions

--- src/features/postListings/PostListing.js
+++ src/features/postListings/PostListing.js
@@ -13,41 +13,45 @@ export default function PostListings({ name, search }) {
   const postsContent = useSelector(selectAllPosts);
   const [queryParams, setQueryParams] = useSearchParams();
 
-  function getFilteredPostIds() {
+  function getCategoryPostIds() {
     const allPostIds = mainListing.postIds;
+    const listingSubs = listing.includedSubs;
+    const categoryPostIds = allPostIds.filter(postId => {
+      // Post's subreddit is within listing's included subreddits
+      const postSub = postsContent[postId].subreddit;
+      return listingSubs.includes(postSub);
+    });
+    return categoryPostIds;
+  }
 
-    if (search) {
-      const query = queryParams.get("q");
-      if (!query) {
-        return allPostIds;
-      }
-
-      const lowerCaseQuery = queryParams.get("q").toLowerCase();
-      const searchPostIds = allPostIds.filter(postId => {
-        // Post's title or subreddit name includes the search term (case-insensitive)
-        const lowerCaseTitle = postsContent[postId].title.toLowerCase();
-        const lowerCaseSub = postsContent[postId].subreddit.toLowerCase();
-        return (lowerCaseTitle.includes(lowerCaseQuery) ||
-                lowerCaseSub.includes(lowerCaseQuery));
-      });
-
-      return searchPostIds;
-
-    } else {
-      const listingSubs = listing.includedSubs;
-      const categoryPostIds = allPostIds.filter(postId => {
-        // Post's subreddit is within listing's included subreddits
-        const postSub = postsContent[postId].subreddit;
-        return listingSubs.includes(postSub);
-      });
-
-      return categoryPostIds;
+  function getSearchPostIds() {
+    const allPostIds = mainListing.postIds;
+    const query = queryParams.get("q");
+    if (!query) {
+      return allPostIds;
     }
+
+    const lowerCaseQuery = queryParams.get("q").toLowerCase();
+    const searchPostIds = allPostIds.filter(postId => {
+      // Post's title or subreddit name includes the search term (case-insensitive)
+      const lowerCaseTitle = postsContent[postId].title.toLowerCase();
+      const lowerCaseSub = postsContent[postId].subreddit.toLowerCase();
+      return (lowerCaseTitle.includes(lowerCaseQuery) ||
+              lowerCaseSub.includes(lowerCaseQuery));
+    });
+    return searchPostIds;
   }
 
   function generatePosts() {
+    let postIds;
+    if (name === "All") {
+      postIds = mainListing.postIds;
+    } else if (search) {
+      postIds = getSearchPostIds();
+    } else {
+      postIds = getCategoryPostIds();
+    }
 
-    const postIds = (name === "All") ? mainListing.postIds : getFilteredPostIds();
     if (postIds.length === 0) {
       return <p>Sorry, no posts available.</p>
     }
