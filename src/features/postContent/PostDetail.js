19ff2c08c5411691b777b23adc24bb2a5c34555b
Chris Mastris <cmastris@hotmail.com>
2023-07-31 21:41:29 +0100

    Redirect non-canonical paths to `commentsPath`

--- src/features/postContent/PostDetail.js
+++ src/features/postContent/PostDetail.js
@@ -1,4 +1,25 @@
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useLocation, useNavigate, useParams } from "react-router-dom";
+
+import { selectPost } from "./postContentSlice";
+import { selectListingsLoadedStatus } from "../postListings/postListingsSlice";
+
 export default function PostDetail() {
+  
+  const { id } = useParams();
+  const listingsLoaded = useSelector(selectListingsLoadedStatus);
+  const post = useSelector(state => selectPost(state, id));
+  
+  // Redirect non-canonical matched paths to `post.commentsPath`
+  const currentPath = useLocation().pathname;
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (listingsLoaded && (currentPath !== post.commentsPath)) {
+      navigate(post.commentsPath);
+    }
+  }, [listingsLoaded, currentPath, navigate, post]);
+
   return (
     <main>
       Post Detail
