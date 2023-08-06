baa05fe70ee870fb840af0913c967478a7baa7b1
Chris Mastris <cmastris@hotmail.com>
2023-08-06 10:45:27 +0100

    Refactor `NavButton` CSS to avoid repetition

--- src/components/NavButton/NavButton.js
+++ src/components/NavButton/NavButton.js
@@ -4,7 +4,8 @@ import styles from "./NavButton.module.css";
 
 export default function NavButton({ anchor, path }) {
   return <NavLink 
-    className={({ isActive }) => isActive ? styles.navButtonActive : styles.navButton}
+    className={({ isActive }) => isActive ?
+      `${styles.navButton} ${styles.navButtonActive}` : styles.navButton}
     to={path}
   >{anchor}</NavLink>;
 }
