ad306b6a285ed9085f9a13439c23cef95e0c27b7
Chris Mastris <cmastris@hotmail.com>
2023-08-06 12:29:36 +0100

    Style `PostListingItem`

--- src/features/postContent/PostListingItem.js
+++ src/features/postContent/PostListingItem.js
@@ -1,22 +1,31 @@
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import { MdChatBubbleOutline, MdArrowUpward } from "react-icons/md";
 
 import { selectPost } from "./postContentSlice";
+import styles from "./PostListingItem.module.css";
 
 export default function PostListingItem({ id }) {
 
   const post = useSelector(state => selectPost(state, id));
   return (
-    <article>
-      <Link to={post.commentsPath}>
-        <div>
-          <div>r/{post.subreddit}</div>
-          <div>{post.category}</div>
-        </div>
-        <h3>{post.title}</h3>
-        <div>
-          <div>{post.upvotes} upvotes</div>
-          <div>{post.commentCount} comments</div>
+    <article className={styles.listing}>
+      <Link className={styles.link} to={post.commentsPath}>
+        <div className={styles.content}>
+          <div className={`${styles.detailRow} ${styles.detailTop}`}>
+            <div>r/{post.subreddit}</div>
+            <div className={styles.divider}>|</div>
+            <div>{post.category}</div>
+          </div>
+          <h3>{post.title}</h3>
+          <div className={`${styles.detailRow} ${styles.detailBottom}`}>
+            <MdArrowUpward className={styles.icon} size={24} />
+            <div className={styles.marginRight}>{post.upvotes} upvotes</div>
+            <div className={styles.comments}>
+              <MdChatBubbleOutline className={`${styles.icon} ${styles.chatIcon}`} size={24} />
+              <div>{post.commentCount} comments</div>
+            </div>
+          </div>
         </div>
       </Link>
     </article>
