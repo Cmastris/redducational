121ce1a85c99bf634f63b25bec2066b3cd58d7ed
Chris Mastris <cmastris@hotmail.com>
2023-08-07 22:07:13 +0100

    Style `Comment`

--- src/features/postContent/Comment.js
+++ src/features/postContent/Comment.js
@@ -1,13 +1,22 @@
 import ReactMarkdown from "react-markdown";
+import { MdArrowUpward, MdPersonOutline } from "react-icons/md";
 
 import MarkdownLinkRenderer from "./MarkdownLinkRenderer";
+import utilStyles from "../../App/utils.module.css";
+import styles from "./Comment.module.css";
 
 export default function Comment({ comment }) {
   return (
-    <article>
-      <div>
-        <div>{comment.scoreHidden ? <span>Score hidden</span> : comment.score}</div>
-        <div>u/{comment.author}</div>
+    <article className={styles.comment}>
+      <div className={`${utilStyles.flexRow} ${utilStyles.darkGrey}`}>
+        <MdArrowUpward className={styles.icon} size={24} />
+        <div className={styles.marginRight}>
+          {comment.scoreHidden ? <span>???</span> : comment.score}
+        </div>
+        <div className={styles.username}>
+          <MdPersonOutline className={`${styles.icon} ${styles.userIcon}`} size={24} />
+          <div>{comment.author}</div>
+        </div>
       </div>
       <div>
         <ReactMarkdown components={{a: MarkdownLinkRenderer}}>{comment.body}</ReactMarkdown>
