2a207943d0cd61359c490c28b12d0d09684c17da
Chris Mastris <cmastris@hotmail.com>
2023-08-08 19:54:07 +0100

    Add homepage link to app name

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 import SearchBar from "../SearchBar/SearchBar";
 import utilStyles from "../../App/utils.module.css";
 import styles from "./Header.module.css";
@@ -5,7 +7,11 @@ import styles from "./Header.module.css";
 export default function Header({ gridArea }) {
   return (
     <header className={styles.header} style={{gridArea}}>
-      <h1 className={styles.appName}><span className={utilStyles.orange}>redd</span>ucational</h1>
+      <div className={styles.appName}>
+        <Link className={styles.link} to="/">
+          <h1 className={styles.h1}><span className={utilStyles.orange}>redd</span>ucational</h1>
+        </Link>
+      </div>
       <SearchBar />
     </header>
   );
